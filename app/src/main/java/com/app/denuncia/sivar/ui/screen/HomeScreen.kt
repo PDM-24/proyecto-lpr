2e0262457a2db0e471275a691302d6ef8c0edec5
wdavid <00135222@uca.edu.sv>
2024-05-01 19:30:32 -0600

    add dropdown menu

--- app/src/main/java/com/app/denuncia/sivar/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/app/denuncia/sivar/ui/screen/HomeScreen.kt
@@ -2,19 +2,16 @@ package com.app.denuncia.sivar.ui.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.app.denuncia.sivar.ui.components.PostComponent.PostComp
 import com.app.denuncia.sivar.ui.components.TopBar.TopAppBarHome
-import com.app.denuncia.sivar.ui.model.PostList
+import com.app.denuncia.sivar.model.PostList
 
 
 @Composable
